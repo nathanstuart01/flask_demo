c946336f51d3052843cd03a2da8d6b08299f16c5
Nathan Stuart <nathan.stuart@ascentialedge.com>
2019-11-14 10:59:11 -0700

    added config class, new pip packages, team model, two routes to get and post team data

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
-#where your data models go when connecting to a database
+from app import db
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    pa_qb = db.Column(db.Float())
+    pa_wr = db.Column(db.Float())
+    pa_rb = db.Column(db.Float())
+    pa_te = db.Column(db.Float())
+
+    def __repr__(self):
+        return '<Team {}, pa_qb {}, pa_wr {}, pa_rb {}, pa_te {}>'.format(self.name, self.pa_qb, self.pa_wr, self.pa_rb, self.pa_te)
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,25 @@
-from app import app
+from app import app, db
+from app.models import Team
+from flask import request, jsonify
 
-@app.route('/')
-def hello_world():
-    return "hello world"
+@app.route('/get_team_data', methods=['GET'])
+def get_all_team_data():
+    team_data = Team.query.all()
+    team_data = [{'team name':team.name, 'points against qb': team.pa_qb, 'points against wr': team.pa_wr, 'points against rb': team.pa_rb, 'points against te': team.pa_te} for team in team_data]
+    return jsonify({'Requested team data': team_data}), 200
 
-@app.route('/football_data')
-def football_data():
-    x = "go seahawks"
-    return x
 
+@app.route('/add_team_data', methods=['POST'])
+def add_team_data():
+    team_name = request.json.get('team_name')
+    pa_qb = request.json.get('pa_qb')
+    pa_wr = request.json.get('pa_wr')
+    pa_rb = request.json.get('pa_rb')
+    pa_te = request.json.get('pa_te')
+
+    team = Team(name=team_name, pa_qb=pa_qb, pa_wr=pa_wr, pa_te=pa_te)
+    db.session.add(team)
+    db.session.commit()
+    return jsonify({'Success':'New team data added', 'team name': team_name, 'points against qb': pa_qb, 'points against wr': pa_wr, 'points against rb': pa_rb, 'points against te': pa_te}), 201
+
+#curl -i -X POST -H "Content-Type: application/json" -d '{"team_name":"team name", "pa_qb":0.0, "pa_wr":0.0, "pa_rb":0.0, "pa_te":0.0}' http://localhost:5000/add_team_data
